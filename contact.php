0f6e45688c935a64c807b8f96f202463ceb029e5
Ayaka Tsubouchi <ayakawell@gmail.com>
2021-01-08 17:29:11 -0800

    changed contact cusomizable

--- contact.php
+++ contact.php
@@ -1,59 +1,66 @@
 <?php
 /*
-Template name: Gallery
+Template name: Contact
 */
 ?>
 <?php get_header(); ?>
 <section class="header-banner">
-    <div style="background-image:linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(http://www.kungfuhotpot.us/wp-content/uploads/2020/05/22.jpg); background-size: cover;
+	<div style="background-image:linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(http://www.kungfuhotpot.us/wp-content/uploads/2020/05/22.jpg); background-size: cover;
   background-repeat: no-repeat;
   background-position: center top;
   height: 300px;">
 
-    </div>
+	</div>
 </section>
 
-<!-- <?php
-        if (have_posts()) :
 
-            while (have_posts()) : the_post();
-                the_content();
-            endwhile;
-
-        else :
-            _e('Sorry, no posts matched your criteria.', 'textdomain');
-
-        endif;
-        ?> -->
 <section class="contact-section">
-	<div class="container my-container">
+	<?php
+	global $post;
+
+	$businessInfo_post = get_posts(array(
+		'post_type' => 'businessInfo',
+		'posts_per_page' => 1,
 
 
-		<div class="row">
-			<div class="col-md-7">
+	));
 
-            <div class="google-map">
-               <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3051.4768247673737!2d-88.2330548843003!3d40.10937608219787!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880cd73f2696216b%3A0xe1b544e62f61f14e!2sKung%20Fu%20BBQ!5e0!3m2!1sen!2sca!4v1609804226267!5m2!1sen!2sca" width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 
-            </div>
-			</div>
-			<div class="col-md-5 text-container">
-				<h3 class="title">Contact Us</h3>
-				<address class="contact-card">
-					<div class="name" itemprop="name">
-						KUNGFU BBQ 功夫火锅 </div>
-					<div class="address">
-						510 E. John St., Champaign IL 61820 </div>
+	if ($businessInfo_post) {
+		foreach ($businessInfo_post as $post) :
+			setup_postdata($post);
 
-					<div class="directions">
-						<a href="//maps.google.com/maps?saddr=current+location&amp;daddr=510+E.+John+St.%2C++Champaign+IL+61820" target="_blank"><i class="fas fa-map-marker-alt"></i>Get directions</a>
+	?>
+			<div class="container my-container">
+
+
+				<div class="row">
+					<div class="col-md-7">
+						<div class="google-map">
+							<?php echo get_field("embed_googld_map", 533); ?>
+
+						</div>
 					</div>
 
-					<div class="phone">
-						<i class="fas fa-mobile-alt"></i>217-355-1888 </div>
-					<div class="opening-hours">
-						<p class="opening-hours-title"><i class="far fa-clock"></i>Opening Hours</p>
-						<table class="opening-hours">
+					<div class="col-md-5 text-container">
+						<h3 class="title">Contact Us</h3>
+						<address class="contact-card">
+							<div class="name" itemprop="name">
+								<?php echo get_field("name", 533); ?></div>
+
+							<div class="address">
+								<?php echo get_field("address", 533); ?></div>
+
+							<div class="directions">
+								<a href="<?php echo get_field("google_map_link", 533); ?>" target="_blank"><i class="fas fa-map-marker-alt"></i>Get directions</a>
+							</div>
+
+							<div class="phone">
+								<i class="fas fa-mobile-alt"></i><?php echo get_field("phone", 533); ?> </div>
+							<div class="opening-hours">
+								<p class="opening-hours-title"><i class="far fa-clock"></i>Opening Hours</p>
+								<?php echo do_shortcode('[mbhi_hours location="Vancouver"]'); ?>
+								<!-- <table class="opening-hours">
 							<tbody>
 								<tr>
 									<th class="weekday-name weekday-monday">Monday</th>
@@ -84,22 +91,27 @@ Template name: Gallery
 									<td class="time">Closed</td>
 								</tr>
 							</tbody>
-						</table>
+						</table> -->
+							</div>
+
+						</address>
+
 					</div>
 
-				</address>
-
+				</div>
 			</div>
-
-		</div>
-	</div>
-	</div>
+			</div>
+	<?php
+		endforeach;
+		wp_reset_postdata();
+	}
+	?>
 </section>
 
-    <a href="#">
-        <div class="back-to-top">
-            <i class="fa fa-angle-up"></i>
-        </div>
-    </a>
+<a href="#">
+	<div class="back-to-top">
+		<i class="fa fa-angle-up"></i>
+	</div>
+</a>
 
-    <?php get_footer(); ?>
+<?php get_footer(); ?>
